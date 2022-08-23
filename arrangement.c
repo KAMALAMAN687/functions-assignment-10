d939bb8c9673fae6d45cce09595346feaf350de8
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-08-23 18:19:11 +0530

    Update arrangement.c

--- arrangement.c
+++ arrangement.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-int combination(int, int);
+int arrangement(int, int);
 int main()
 {
     int n,r,s;
     printf("enter a number of items and enter items selected at a time: ");
     scanf("%d%d",&n,&r);
-    s=combination(n,r);
-    printf("no. of combinations are : %d",s);
+    s=arrangement(n,r);
+    printf("no. of arrangement are : %d",s);
     return 0;
 
 }
-int combination(int a, int b)
+int arrangement(int a, int b)
 {
     int c,d=1,e,f,g=1;
     //calculate factorial of n
